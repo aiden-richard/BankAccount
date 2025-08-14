a6f23dddbfdc14e3ba659c4e21be447d76052cd6
aiden-richard <richardaiden2006@gmail.com>
2025-08-14 15:35:45 -0700

    Make Validator class

---
+++ BankAccount/Validator.cs
@@ -0,0 +1,20 @@
+﻿namespace BankAccount;
+
+public class Validator
+{
+    /// <summary>
+    /// Checks to see if a value is within a specified range (inclusive)
+    /// </summary>
+    /// <returns></returns>
+    public bool IsWithinRange(double value, double min, double max)
+    {
+        if (min < value && value < max)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
