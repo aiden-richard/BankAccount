68e0150b7640259d0081df332d6a55c96aa28eb2
aiden-richard <richardaiden2006@gmail.com>
2025-08-14 16:09:25 -0700

    Add min and max value checks in IsWithininRange method

--- BankAccount/Validator.cs
+++ BankAccount/Validator.cs
@@ -6,9 +6,16 @@ public class Validator
     /// Checks to see if a value is within a specified range (inclusive)
     /// </summary>
     /// <returns></returns>
+    /// <exception cref="ArgumentException">Throws if min > max</exception>
     public bool IsWithinRange(double value, double min, double max)
     {
-        if (min < value && value < max)
+        if (min > max)
+        {
+            throw new ArgumentException("Min cannot be greater than max.");
+        }
+
+
+        if (min <= value && value <= max)
         {
             return true;
         }
